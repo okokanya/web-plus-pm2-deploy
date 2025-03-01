2491d361a6e2a4adc41b740fe819f38b68d4b6e9
anya pushkina <okokanya@gmail.com>
2025-03-01 18:28:17 +0800

    dep

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,16 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env.deploy" });
-const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_PATH,
-  DEPLOY_REF,
-  DEPLOY_REPOSITORY } = process.env;
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF } =
+  process.env;
+
 module.exports = {
-  apps : [{
-    name   : "mesto",
-    script : "dist/app.js"
-  }],
+  apps: [
+    {
+      name: "mesto",
+      script: "dist/app.js",
+    },
+  ],
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -19,8 +19,7 @@ module.exports = {
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
-      // 'post-deploy': 'npm i && npm run build',
+      "post-deploy": "cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
     },
   },
-}
+};
